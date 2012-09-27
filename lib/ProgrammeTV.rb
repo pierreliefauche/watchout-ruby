f19ccd67d791e73351712dd4fcb6ab5cae040272
Pierre-Elie Fauche <pierre-elie@jolicloud.com>
2012-09-27 19:32:17 +0200

    Fix for new version of www.programme.tv

--- lib/ProgrammeTV.rb
+++ lib/ProgrammeTV.rb
@@ -1,25 +1,14 @@
 # encoding: utf-8
 require 'curb'
 
-class ProgrammeTV 
+class ProgrammeTV
 	
 	def wday_from_label(label)
-		case label
-			when 'dimanche'
-				0
-			when 'lundi'
-				1
-			when 'mardi'
-				2
-			when 'mercredi'
-				3
-			when 'jeudi'
-				4
-			when 'vendredi'
-				5
-			when 'samedi'
-				6
-		end
+	  ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'].index(label).to_i
+	end
+	
+	def channel_id_from_name(name)
+		['0', 'tf1', 'france2', 'france3', 'canalplus', 'france5', 'm6', 'arte', 'direct8', 'w9', 'tmc', 'nt1', 'nrj12', 'lcp-senat', 'france4', 'bfmtv', 'itele', 'direct-star', 'gulli', 'franceO'].index(name)
 	end
 	
 	def night(wday_label)
@@ -31,128 +20,107 @@ class ProgrammeTV
 		channels = settings.cache.get(cache_label)
 		
 		if (!channels)
-			easy = Curl::Easy.http_get("http://www.programme.tv/soiree/#{wday_label}.php")
+		  easy = Curl::Easy.new("http://www.programme.tv/tnt/soiree/#{wday_label}.php") do |c|
+		    c.follow_location = true
+		    c.max_redirects = nil
+		  end
+		  easy.http_get
 			page = easy.body_str
 			easy.close
 				
 			# suppression de ce qu'il y a avant et après la liste des programmes
-			page.gsub(/\n/, '') =~ /<table class=\"contenu_soiree\">[^<]*<tr>(.*)<div id=\"droite\">/
-			page = $1.gsub(/<script[^<]*script>[^<]*<\/td>/, '</td>').force_encoding("UTF-8")
+			page.gsub(/\n/, '') =~ /<ul class=\"left\">(.*<ul class=\"right\">.*?)<\/ul>/
+  		page = $1#.force_encoding("UTF-8")
 			
 			channels = Hash.new
 			
-			page.split(/<td valign=\"top\"[^>]*class=\"soiree[^\"]*\">/).each do |line|
-	# 			matches:
-	# 			1: chaine
-	# 			2: heure de début du programme
-	# 			3: lien pour le programme
-	# 			4: programme
-				channels[$1.to_i] = {
-					'id' => $1.to_i,
-					'showLink' => $3,
-					'show' => $4,
-					'showStart' => $2.sub('.', 'h'),
-				} if line =~ /chaine\/([0-9]{1,2})p?\.gif.*([0-9]{2}\.[0-9]{2}).*href=\"([^\"]*)\" class=\"bb12\">([^<]*)/
+			page.split(/<li class=\"box\">/i).each do |line|
+				channels[channel_id_from_name($1)] = {
+					'id' => channel_id_from_name($1),
+          'link' => "http://www.programme.tv#{$3}",
+          'title' => $4.strip,
+          'start' => $2,
+				} if line =~ /href=\"http:\/\/old.programme.tv\/chaine\/([^\/]+)\/\".*<span class=\"hour\">([0-9]{2}:[0-9]{2})<\/span>.*<h3>\s*(?:<a href=\"([^"]*)\"[^>]*>)?([^<]*)(?:<\/a>)?\s*<\/h3>/
 			end
-			settings.cache.set(cache_label, channels)
+      settings.cache.set(cache_label, channels)
 		end
 	
 		channels
 	end
 	
 	def now
-		easy = Curl::Easy.http_get("http://www.programme.tv/actuellement/")
+		easy = Curl::Easy.http_get("http://www.programme.tv/actuellement/tnt/")
 		page = easy.body_str
 		easy.close
-			
+		
 		# suppression de ce qu'il y a avant et après la liste des programmes
-		page.gsub(/\n/, '') =~ /<table class=\"table_actu\">(.*)<div id=\"droite\">/
+		page.gsub(/\n/, '') =~ /<ul class=\"left\">(.*<ul class=\"right\">.*?)<\/ul>/
 		page = $1.force_encoding("UTF-8")
 		
-		channels = Array.new
+		channels = Hash.new
 		
-		page.split(/<tr>\s*<td class=\"width50p( bar\_gris)?\">/i).each do |line|			
-	# 		matches:
-	# 		1: chaine 
-	# 		2: lien pour le programme actuel
-	# 		3: programme actuel
-	# 		4: heure début programme actuel
-	# 		5: pourcentage d'avancement du programme actuel
-	# 		6: heure fin programme actuel
-	# 		7: heure début programme qui suit
-	# 		8: lien pour le programme qui suit
-	# 		9: programme qui suit
-	# 		10: heure début programme d'encore après
-	# 		11: lien pour le programme d'encore après
-	# 		12: programme d'encore après
-			channels.push({
-				'id' => $1.to_i,
-				'nowLink' => $2,
-				'now' => $3,
-				'nowStart' => $4,
-				'nowEnd' => $6,
-				'nowDone' => $5,
-				'next' => $9,
-				'nextStart' => $7,
-				'nextLink' => $8,
-				'nextNext' => $12,
-				'nextNextStart' => $10,
-				'nextNextLink' => $11,
-			}) if line =~ /chaine\/([0-9]{1,2})p?\.gif.*href=\"([^\"]*)\".*<b>(.*)<\/b>.*([0-9]{2}h[0-9]{2}).*actu_chiffre\">([0-9]{1,3})%<\/span>.*&nbsp;([0-9]{2}h[0-9]{2}).*([0-9]{2}h[0-9]{2}).*href=\"([^\"]*)\".*<b>(.*)<\/b>.*([0-9]{2}h[0-9]{2}).*href=\"([^\"]*)\".*<b>(.*)<\/b>/
+		page.split(/<li class=\"box\">/i).each do |line|
+			channels[channel_id_from_name($1)] = {
+				'id' => channel_id_from_name($1),
+				'start' => $2,
+				'link' => "http://www.programme.tv#{$3}",
+				'title' => $4.strip,
+				'end' => $5,
+				'percent' => $6,
+			} if line =~ /href=\"http:\/\/old.programme.tv\/chaine\/([^\/]+)\/\".*<span class=\"hour\">([0-9]{2}:[0-9]{2})<\/span>.*<h3>\s*(?:<a href=\"([^"]*)\"[^>]*>)?([^<]*)(?:<\/a>)?\s*<\/h3>.*<span class=\"progressbar-end\">([0-9]{2}:[0-9]{2})<\/span>.*<div class=\"pr?ogressbar-percent\">([0-9]{1,3})%<\/div>/
 		end
 	
 		channels		
 	end
 	
 	def show(url)
-		show = settings.cache.get(url)
+    show = settings.cache.get(url)
 		
 		if !show
 			easy = Curl::Easy.http_get(url)
 			page = easy.body_str
 			easy.close
-		
+
 			# suppression de ce qu'il y a avant et après ce qui nous intéresse
-			page.gsub(/\n/, '') =~ /table width=\"100%\" class=\"eTable\">(.*)<td class=\"b12\">Les autres diffusions/
+			page.gsub(/\n/, '') =~ /<div id=\"progselect\">(.*)<div id=\"pubcontent\">/
 			page = $1.force_encoding("UTF-8")
 			
 			show = Hash.new
 			
 			# récupération du type du programme avec son pays et année de production
-			show['type'] = $1 if page =~ /<td colspan=\"2\" align=\"right\" class=\"b12 efond\">(.*)&nbsp;<\/td>/
+			show['type'] = $1 if page =~ /<span class=\"type[^"]*\">\s*([^<]*)\s*/
 			
-			# récupération des horaires exacts
-			begin 
-				show['start'] = $1;
-				show['end'] = $2;
-			end if page =~ /<span class=\"eHeure\">([0-9]{2}h[0-9]{2})<\/span>.*([0-9]{2}h[0-9]{2})<br \/>/
+			# récupération des horaires			
+			show['start'] = $1 if page =~ /<span class=\"date\">[^<]*?([0-9]{2}h[0-9]{2})<\/span>/
+			show['duration'] = $1 if page =~ /<span class=\"infos\">Durée : ([^<]*)<\/span>/
 			
 			# récupération du titre du programme
-			show['title'] = $1 if page =~ /<div class=\"b18\">([^<]*)</
+			show['title'] = $1 if page =~ /<h1>([^<]*)/
 			
 			# récupération du numéro de programme pour l'image
-			show['image'] = $1 if page =~ /<td class=\"center\">[^<]*<img src=\"([^>]*\.jpg)\"/
+			show['image'] = $1 if page =~ /\"illustration_1\".*?<img src=\"([^"]*)/
 			
 			# récupération du sous-titre du programme
-			show['subtitle'] = $1 if page =~ /<\/div><div class=\"b12\">(.+)<\/div>/
+			show['subtitle'] = $1 if page =~ /<h2>([^<]*)/
 			
 			# récupération de la saison et de l'épisode (applicable seulement aux séries)
-			if page =~ /<br \/>Saison : ([0-9]{1,3})[^<]*<br \/>Episode : ([0-9\/]{1,9})[^<]*<br \/>/
+
+			if page =~ /<span class=\"infos\">Saison : ([0-9]{1,3}) - Episode : ([0-9\/]{1,9})</
 				show['season'] = $1
 				show['episode'] = $2
 			end
 	
 			# récupération du résumé
-			if page =~ /<span id=\"intelliTXT\">(.*)<\/?span\/?>/
+			if page =~ /<div class=\"resume\">.*?<p>(.*?)<\/p>/
 				texte = $1
 				# suppression des infos qu'on ne veut pas
 				texte.gsub!(/(<b>Invit[^s]*s :<\/b>[^<]*<br \/><br \/>)|(<b>Pr[^s]*sent[^ ]* par :<\/b>[^<]*<br \/><br \/>)|(<b>R[^a]*alis[^ ]* par :<\/b>[^<]*<br \/><br \/>)|(<b>Acteurs :<\/b>[^<]*<br \/><br \/>)|(<b>Notre avis :<\/b>[^<]*<br \/><br \/>)|(<br \/>)/, '')
-				texte.gsub!(/<b>/, '<br/><b>')
-				texte.gsub!(/<\/b>/, '</b><br/>')
-				texte.gsub!(/<br\/><br\/>/, '<br/>')
+				texte.gsub!(/<span class=\"bold\">/, '<br/><b>')
+				texte.gsub!(/<\/span>/, '</b><br/>')
+				texte.gsub!(/<br\/?><br\/?>/, '<br/>')
 				show['resume'] = texte
 			end
-			settings.cache.set(url, show)
+      settings.cache.set(url, show)
 		end
 		
 		show
